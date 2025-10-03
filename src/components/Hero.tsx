5c3083cc27d5e94d021b1833c4524adc5a724b64
yusufasici <yusuf.asici@gmail.com>
2025-10-03 01:46:28 -0400

    fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ const Hero = () => {
           
           {/* Logo Mascot*/}
           <div className="flex items-center justify-center">
-            <img src={logomascot} className="h-56 w-auto opacity-75 object-contain" />
+            <img src={logomascot} className="h-44 w-auto opacity-75 object-contain" />
           </div>
 
           {/* Subtitle */}
