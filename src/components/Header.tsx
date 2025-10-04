75933b2e91b9f7f59e4075435ce86830fa9be7b6
yusufasici <yusuf.asici@gmail.com>
2025-10-04 15:00:47 -0400

    face

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -125,7 +125,7 @@ const Header = () => {
                 <Instagram className="h-6 w-6" />
               </a>
               <a 
-                href="https://facebook.com/s" 
+                href="https://www.facebook.com/profile.php?id=61581941300247" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="text-primary hover:text-primary-glow transition-colors"
