b7db84c3da9c716a14f052124d5ed752c8cb98bd
yusufasici <yusuf.asici@gmail.com>
2025-10-06 23:11:39 -0400

    HotFix

--- src/pages/services/Residential.tsx
+++ src/pages/services/Residential.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ServicesPage from "./ServicesPage";
 import heroHome from "@/assets/residential-lock.jpg";
 
+const residentialServices = [
+  "House Locked Out",
+  "Lock Change",
+  "Lock Rekeying",
+  "Lock Repair",
+  "Lock Installation",
+  "Mailbox Lock Change",
+];
+
 const Residential: React.FC = () => (
   <ServicesPage
     title="Residential Locksmith Services"
     description="Home lockouts, rekeying, lock installation, and security upgrades. Protect your family and property with trusted local experts."
     heroImage={heroHome}
+    services={residentialServices}
   >
     {/* Add detailed residential locksmith content here */}
     <div className="prose mx-auto">
